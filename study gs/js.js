8218653f83e8b114592bbdf2a7c31b54190f58a4
RVolskyi <rvolskyi@gmail.com>
2016-11-27 23:10:45 +0200

    add lern js catalog

---
+++ study gs/js.js
@@ -0,0 +1,76 @@
+/**
+ * Created by Roma on 27.11.2016.
+ */
+
+
+
+
+/*var user;
+
+user = prompt('enter your name');
+
+if (user == 'admin') {
+    alert ('Hello admin');
+} else {
+    alert ('go awey');
+}
+
+
+var arr = [2, 7, 'fdfdf', true];
+
+console.log('arr', arr);
+console.log('длинна', arr.length);
+
+var addElem = prompt('add your element');
+arr.push(addElem);
+console.log('arr', arr);
+console.log('длинна', arr.length);
+
+
+
+var removedItem = arr.pop();
+
+console.log('view arrPop',removedItem);
+console.log('arr', arr);
+console.log('длинна', arr.length);
+
+arr.unshift('first add');
+console.log('arr', arr);
+console.log('длинна', arr.length);
+
+arr.shift();
+console.log('arr', arr);
+console.log('длинна', arr.length);*/
+
+
+/*var i = 1;
+
+    // while (i < 5) {
+    //     alert('test');
+    //     console.log('i=', i);
+    //     i++;
+    // }
+do {
+    console.log('i = ', i);
+    alert('help ' + i);
+    i++;
+} while (i < 5);*/
+
+// var arr = [2, 7, 'fdfdf', true];
+// console.log(arr);
+//
+// for (var i = arr.length - 1; i >= 0; i--) {
+//     debugger;
+//     console.log('arr [i=]', arr[i]);
+//     console.log('i = ' + i);
+// }
+//
+// alert('DONE');
+
+
+
+var arr = [1, 2, 3];
+console.log(arr);
+arr[arr.length] = 4;
+console.log(arr);
+
